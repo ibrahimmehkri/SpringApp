beabfcb058dc6346494394f36b70e89f0c3637a2
zmehibr <ibrahimmehkri@gmail.com>
2026-02-20 13:56:11 +0100

    Added a new endpoint

--- mypackage/MyController.java
+++ mypackage/MyController.java
@@ -10,4 +10,9 @@ public class MyController {
     public String hello() {
         return "Hello World";
     }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome aboard!"
+    }
 }
